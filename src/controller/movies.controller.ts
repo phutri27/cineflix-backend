80c638a51d2fa458e3bbe99d1e3bb8b99778e8d6
phutri27 <triphu27@gmail.com>
2026-02-25 18:36:35 +0000

    Create post, delete, update route and implement movie controller insert and update function

--- src/controller/movies.controller.ts
+++ src/controller/movies.controller.ts
@@ -1,6 +1,8 @@
 import { moviesObj } from "../dao/movies.dao";
 import type { NextFunction, Request, Response } from "express";
 import { matchedData, validationResult, Result } from "express-validator";
+import { validateMovie } from "../validate/movies.validate";
+import { uploadFile } from "../utils/fileupload";
 
 export const getAllComingMovies = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -24,7 +26,7 @@ export const getAllShowingMovies = async (req: Request, res: Response, next: Nex
 
 export const getMoviesByGenre = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const genre = req.params.genre as string
+        const genre = req.params.genre_name as string
         const movies = await moviesObj.getMoviesByGenre(genre)
         return res.status(200).json(movies)
     } catch (error) {
@@ -35,7 +37,7 @@ export const getMoviesByGenre = async (req: Request, res: Response, next: NextFu
 
 export const getMoviesByTitle = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const title = req.params.title as string
+        const title = req.params.title_name as string
         const movies = await moviesObj.getMoviesByTitle(title)
         return res.status(200).json(movies)
     } catch (error) {
@@ -44,19 +46,44 @@ export const getMoviesByTitle = async (req: Request, res: Response, next: NextFu
     }
 }
 
-export const insertAndUpdateMovies = async (req: Request, res: Response, next: NextFunction) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()){
-        const result: Result<string> = errors.formatWith(error => error.msg as string)
-        return res.status(400).json({errors: result.array()})
-    }
-    
-    
-    const { title, plot, duration, premiere_date, rated } = matchedData(req)
+export const insertMovies = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        
+        const filePath = req.file?.path as string
+        const { title, plot, duration, genre_option, actors, directors, premiere_date, rated } = matchedData(req)
+        const imageUrl: any = await uploadFile(filePath)
+        const movie = await moviesObj.insert(title, plot, imageUrl.public_id, duration, premiere_date, rated,
+            genre_option, actors, directors
+        )
+        return res.status(200).json(movie)
+    } catch (error) {
+        console.error(error)
+        return next(error)
+    }
+}
+
+export const updateMovies = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const id = req.params.id as string
+        const filePath = req.file?.path as string
+        const { title, plot, duration, genre_option, actors, directors, premiere_date, rated } = matchedData(req)
+        const imageUrl: any = await uploadFile(filePath)
+        const movie = await moviesObj.update(id, title, plot, imageUrl.public_id, duration, premiere_date, rated,
+            genre_option, actors, directors
+        )
     } catch (error) {
         
     }
 }
 
+export const deleteMovie = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const id = req.params.id as string
+        const movie = await moviesObj.delete(id)
+        return res.status(200).json({
+            message: "Delete movie succesfully"
+        })
+    } catch (error) {
+        console.error(error)
+        return next(error)
+    }
+}
--- src/routes/movies.route.ts
+++ src/routes/movies.route.ts
@@ -1,10 +1,19 @@
 import express from "express"
 import * as movies from "../controller/movies.controller.js"
+import { validateMovie } from "../validate/movies.validate.js"
+import { handleValidationErrors } from "../middlewares/validateResult.js"
+import { upload } from "../utils/fileupload.js"
 const router = express.Router()
 
 router.get("/coming_movies", movies.getAllComingMovies)
 router.get("/showing_movies", movies.getAllShowingMovies)
-router.get("/:genre", movies.getMoviesByGenre)
-router.get("/:title", movies.getMoviesByTitle)
+router.get("/genre/:genre_name", movies.getMoviesByGenre)
+router.get("/title/:title_name", movies.getMoviesByTitle)
 
+
+router.post("/movie",upload.single('filename'), validateMovie, handleValidationErrors, movies.insertMovies)
+
+router.put("/movie/:id", upload.single('filename'), validateMovie, handleValidationErrors, movies.updateMovies)
+
+router.delete("/movie/:id", movies.deleteMovie)
 export default router
