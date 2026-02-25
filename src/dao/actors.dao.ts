dca4caa675e5c9dc7f3eefc1ad960fc41e849307
phutri27 <triphu27@gmail.com>
2026-02-25 08:40:57 +0000

    add actor query implementation

---
+++ src/dao/actors.dao.ts
@@ -0,0 +1,38 @@
+import { prisma } from "../lib/prisma";
+
+class Actors {
+    async insert(name: string){
+        const actor = await prisma.actor.create({
+            data:{
+                name: name
+            }
+        })
+
+        return actor
+    }
+
+    async update(id: string, name: string){
+        const actor = await prisma.actor.update({
+            data:{
+                name: name
+            },
+            where:{
+                id: id
+            }
+        })
+
+        return actor
+    }
+
+    async delete(id: string){
+        const actor = await prisma.actor.delete({
+            where:{
+                id: id
+            }
+        })
+
+        return actor
+    }
+}
+
+export const actorObj = new Actors()
