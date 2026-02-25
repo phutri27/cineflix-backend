1dff47e5e7f70cf99f9aadba8ed20f5dc199b492
phutri27 <triphu27@gmail.com>
2026-02-25 08:41:10 +0000

    add genre query implementation

---
+++ src/dao/genres.dao.ts
@@ -0,0 +1,43 @@
+import { prisma } from "../lib/prisma";
+
+class Genres{
+    async insert(name: string){
+        const genre = await prisma.genre.create({
+            data:{
+                name: name
+            }
+        })
+
+        return genre
+    }
+
+    async update(name: string, id: string){
+        const genre = await prisma.genre.update({
+            data:{
+                name: name
+            },
+            where:{
+                id: id
+            }
+        })
+
+        return genre
+    }
+
+    async getAllGenres(){
+        const genres = await prisma.genre.findMany()
+        return genres
+    }
+
+    async delete(id: string){
+        const genre = await prisma.genre.delete({
+            where:{
+                id: id
+            }
+        })
+
+        return genre
+    }
+}
+
+export const genreObj = new Genres()
