05a702454b322803bdf821c1613aa56c22320b2c
phutri27 <triphu27@gmail.com>
2026-05-08 01:08:55 +0700

    fixL add readOnly false to worker connection

--- src/config/redis-worker-connection.ts
+++ src/config/redis-worker-connection.ts
@@ -4,5 +4,6 @@ import IORedis from "ioredis"
 export const redisWorkerConnection = new IORedis({
     host: process.env.REDIS_HOST,
     port: Number(process.env.REDIS_PORT) || 6379,
-    maxRetriesPerRequest: null
+    maxRetriesPerRequest: null,
+    readOnly: false
 })
