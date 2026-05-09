7cd9c45ce56ef6631ff14a14adee5476fd4798ff
phutri27 <triphu27@gmail.com>
2026-05-09 22:45:36 +0700

    fix: redis-worker-connection

--- src/config/redis-worker-connection.ts
+++ src/config/redis-worker-connection.ts
@@ -5,5 +5,4 @@ export const redisWorkerConnection = new IORedis({
     host: process.env.REDIS_HOST,
     port: Number(process.env.REDIS_PORT) || 6379,
     maxRetriesPerRequest: null,
-    readOnly: false
 })
